de07fb9e91d66aa1e15816ad5d5239644de9bed6
Christian Kleinboelting <christian@kleinboelting.de>
2018-05-31 15:03:50 +0200

    Add test for default ActorSystem name

--- src/test/java/de/dynamight/akka/junit/AkkaJunitExtensionWithMaterializerTest.java
+++ src/test/java/de/dynamight/akka/junit/AkkaJunitExtensionWithMaterializerTest.java
@@ -1,5 +1,6 @@
 package de.dynamight.akka.junit;
 
+import akka.actor.ActorSystem;
 import akka.stream.ActorMaterializer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,8 +8,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(AkkaJunitExtension.class)
-@AkkaJunitExtensionConfig(name = "test")
+@AkkaJunitExtensionConfig()
 class AkkaJunitExtensionWithMaterializerTest {
+  
+  @Test
+  void actorsystem_has_default_name(ActorSystem system) {
+    assertThat(system.name()).isEqualTo("default");
+  }
 
   @Test
   void actormaterializer_is_present(ActorMaterializer materializer) {
